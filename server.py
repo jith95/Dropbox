bad68939fadf61b2af1244d99f3b32f4a7201f8d
Sreejith Srikrishnan <sreejith.srikrishnan@gmail.com>
2018-11-21 22:32:20 +0530

    modified it with the master's server.py

--- server.py
+++ server.py
@@ -13,7 +13,9 @@ def client_thread(connection, ip, port, max_buffer_size = 5120):
 
     while clientActive:
         client_input = receive_input(connection, max_buffer_size)
+
         if "--QUIT--" in client_input:
+            
 			print("Client is requesting to quit")
             connection.close()
             print("Connection " + ip + ":" + port + " closed")
