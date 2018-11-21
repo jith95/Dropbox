a43396630c4b0aec3340b02000652810a6aee787
Sreejith Srikrishnan <sreejith.srikrishnan@gmail.com>
2018-11-21 22:39:24 +0530

    new file added login.py

---
+++ login.py
@@ -0,0 +1,26 @@
+print("Welcome...")
+welcome = input("Do you have an acount? y/n: ")
+if welcome == "n":
+    while True:
+        username  = input("Enter a username:")
+        password  = input("Enter a password:")
+        password1 = input("Confirm password:")
+        if password == password1:
+            file = open(".txt", "w")
+            file.write(username+":"+password)
+            file.close()
+            welcome = "y"
+            break
+        print("Passwords do NOT match!")
+ 
+if welcome == "y":
+    while True:
+        login1 = input("Login:")
+        login2 = input("Password:")
+        file = open(login1+".txt", "r")
+        data   = file.readline()
+        file.close()
+        if data == login1+":"+login2:
+            print("Welcome")
+            break
+        print("Incorrect username or password.")
