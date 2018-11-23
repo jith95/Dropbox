36ec3b6762b3b3e21fc634688262615897da47a6
Sreejith Srikrishnan <sreejith.srikrishnan@gmail.com>
2018-11-23 16:03:14 +0530

    Modified the login.py file .

--- login.py
+++ login.py
@@ -1,27 +1,45 @@
-print("Welcome...")
-welcome = input("Do you have an acount? y/n: ")
-if welcome == "n":
+d = {}
+with open(r"C:\Users\SREEJITH\Desktop\IITGN\DB\Database.txt") as f:
+    for line in f:
+       (key, val) = line.split()
+       d[key] = val
+def signup():
     while True:
         username  = input("Enter a username:")
         password  = input("Enter a password:")
         password1 = input("Confirm password:")
         if password == password1:
             file = open(r"C:\Users\SREEJITH\Desktop\IITGN\DB\Database.txt", "a+")
-            file.write(username+":"+password)
+            #file = open(r"file.txt", "a+")
+            file.write(username+" "+password+"\n")
             file.close()
             welcome = "y"
             break
         print("Passwords do NOT match!")
- 
-if welcome == "y":
+def login():
+    s=0
     while True:
         login1 = input("Login:")
         login2 = input("Password:")
         file = open(r"C:\Users\SREEJITH\Desktop\IITGN\DB\Database.txt", "r")
-        data   = file.readline()
-        file.close()
-        print(data)
-        if data == login1+":"+login2:
+        #file = open(r"file.txt", "r")
+        for i in d:
+            if i==login1 and d[i]==login2:
+                s=1
+                break;
+        if(s==1):
             print("Welcome")
+            
             break
-        print("Incorrect username or password.")
+        else:
+            print("Passwords don't match")
+            
+
+print("Welcome...")
+welcome = input("Do you have an acount? y/n: ")
+if welcome == "n":
+    signup()
+ 
+if welcome == "y":
+    d=login()
+        
